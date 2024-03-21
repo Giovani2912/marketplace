484493146a71725e4f3c8b4f9507f0a9d89a3a71
Giovani-Foltran <giovanicanovaf@gmail.com>
2024-03-21 16:39:58 -0300

    final changes

--- next.config.js
+++ next.config.js
@@ -1,16 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		remotePatterns: [
-			{
-				protocol: "http",
-				hostname: "localhost",
-			},
-			{
-				protocol: "https",
-				hostname: "digitalhippo-production.up.railway.app",
-			},
-		],
+			domains:[
+				"https",
+				"marketplace-production-d946.up.railway.app",
+			]
 	},
 };
 
